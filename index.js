f3fc247064e9fe195898bb7872f0b25cf0f2bcbd
Sahil V N <iss225@iss2.local>
2023-05-09 14:57:08 +0530

    social media backend

---
+++ index.js
@@ -0,0 +1,62 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import multer from 'multer';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import userRoutes from './routes/user.js';
+import postRoutes from './routes/post.js';
+import { register } from './controllers/auth.js';
+import { createPost } from './controllers/post.js';
+import { verifyToken } from './middleware/auth.js';
+
+/** CONFIGURATIONS*/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+
+/** FILE STORAGE */
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+/** ROUTES WITH FILES */
+app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
+
+/**ROUTES */
+app.use('/routes', authRoutes);
+app.use('/users', userRoutes);
+
+/** MONGOOSE SETUP*/
+const PORT = process.env.PORT || 6000;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`server connected to port:${PORT}`));
+
+   
+  })
+  .catch((err) => console.log(`${err} did not connect`));
